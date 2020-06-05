f29be9b3f4ade783db91dc0e8d5c8c6e127519fa
Sofia Modig <sofiamodig1@gmail.com>
2020-06-05 10:01:17 +0200

    bugfix transition, changes in IE

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,21 +1,42 @@
 import React, { Component }  from "react"
 import Img from "gatsby-image"
 import { Link } from "gatsby"
+import { isIE } from "react-device-detect"
 
 import PrimaryButton from "./buttons/primary.js"
 
 import Styles from "./hero.module.css"
 
 class Hero extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+      activeClass: ""
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      activeClass: Styles.animate_hero
+    })
+  }
+
   render() {
     const { heading, text, img, alt, button, buttonlink, shorttitle, parentPageLink, parentPageTitle, lowerImg, startpage, fade } = this.props
 
     let heroImage
-    if (img && img.extension === "svg") {
+    if (img && img.extension === "svg" && isIE) {
+      heroImage = 
+      <div className={Styles.hero_section_image + " " + (lowerImg ? Styles.lower_img : "" )}>
+        <img src={img.publicURL} alt={alt} />
+      </div>
+    
+    } else if (img && img.extension === "svg") {
       heroImage = 
       <div className={Styles.hero_section_image + " " + (lowerImg ? Styles.lower_img : "" )}>
         <object type="image/svg+xml" data={img.publicURL} aria-labelledby={alt}></object>
       </div>
+
     } else if (img) {
       heroImage = 
       <div className={Styles.hero_section_image + " " + (lowerImg ? Styles.lower_img : "" )}>
@@ -27,7 +48,7 @@ class Hero extends Component {
     }
 
     return (
-      <div className={Styles.hero_section + " " + (fade ? Styles.fade : "")}>
+      <div className={Styles.hero_section + " " + (fade ? Styles.fade : "") + " " + this.state.activeClass}>
         <div className={Styles.hero_row + " " + (startpage ? Styles.startpage : "")}>
           <div className={Styles.hero_section_text}>
             {((parentPageLink || shorttitle) || (parentPageLink && shorttitle)) &&
@@ -41,8 +62,8 @@ class Hero extends Component {
                 }
               </div>
             }
-            <h1 className={Styles.h1_animation}>{heading}</h1>
-            <p className={Styles.p_animation}>{text}</p>
+            <h1>{heading}</h1>
+            <p>{text}</p>
 
             <div className={Styles.btn_animation}>
               {button &&
