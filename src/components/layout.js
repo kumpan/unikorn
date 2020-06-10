194a4e642671c9679c0966e2cd626d3175423b79
Sofia Modig <sofiamodig1@gmail.com>
2020-06-10 14:24:31 +0200

    cookie test

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React, { Component }  from "react"
-import { Link } from "gatsby"
-import CookieConsent from "react-cookie-consent";
-import CloseIcon from "../../content/assets/icons/close.svg"
+// import { Link } from "gatsby"
+// import CookieConsent from "react-cookie-consent";
+// import CloseIcon from "../../content/assets/icons/close.svg"
 
 import Nav from "./nav"
 import Footer from "./footer"
@@ -15,10 +15,10 @@ class Layout extends Component {
         <Nav location={this.props.location}/>
         <main>{children}</main>
         <Footer show_contact_info={this.props.show_contact_info} />
-        <CookieConsent buttonText="OK" cookieName="cookieConcent" contentClasses="cookie-text" buttonWrapperClasses="cookie-btn">
+        {/* <CookieConsent buttonText="OK" cookieName="cookieConcent" contentClasses="cookie-text" buttonWrapperClasses="cookie-btn">
           This website uses cookies to ensure you get the best experience. <Link to="/cookie-policy">Read the policy here</Link>.
           <CloseIcon />
-        </CookieConsent>
+        </CookieConsent> */}
       </div>
     )
   }
