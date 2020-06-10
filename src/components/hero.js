b5064d86da1452977ee9d0ce8dd7eebf400bc88f
Sofia Modig <sofiamodig1@gmail.com>
2020-06-10 11:49:36 +0200

    change cssvars to only run on legacy

--- src/components/hero.js
+++ src/components/hero.js
@@ -31,7 +31,7 @@ const Hero = (props) => {
   }
 
   return (
-    <div className={Styles.hero_section + " " + (fade ? Styles.fade : "") + " hero-animation"}>
+    <div className={Styles.hero_section + " " + (fade ? Styles.fade : "") + " " + Styles.animate_hero + " " + (isIE ? Styles.isIE : "")}>
       <div className={Styles.hero_row + " " + (startpage ? Styles.startpage : "")}>
         <div className={Styles.hero_section_text}>
           {((parentPageLink || shorttitle) || (parentPageLink && shorttitle)) &&
